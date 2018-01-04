4955b3d2312fe79ff2c892df67200722771f9b5c
Fernando L. Canizo <fernandocanizo@gmail.com>
2018-01-04 12:46:18 -0300

    Initial empty app running

---
+++ agni.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.get('/about', (req, res) => res.render('pages/about'));
+
+app.listen(8080);
+console.log('Listening on 8080 is the magic port');
